1707855b224ae99cba93fa5c5f1f1ddeb2dc0cd6
wrzonki <wrzonki@gmail.com>
2023-04-13 00:21:20 +0200

    first commit 🐶🐱

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<a href="/pet/luna">luna</a>
+<a href="/adopcja">adopcja</a>
+<a href="/schronisko">schronisko</a>
+<a href="/wolontariat">wolontariat</a>
+<slot />
---
+++ src/routes/pet/[pet]/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+	import { page } from '$app/stores';
+	const { pet } = $page.params;
+</script>
+
+<h2>{pet}</h2>
