d84e94f6825f3ba320b2c462ceaa86424e871a4b
wrzonki <wrzonki@gmail.com>
2023-04-13 00:39:03 +0200

    added basic nav

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,34 @@
-<a href="/pet/luna">luna</a>
-<a href="/adopcja">adopcja</a>
-<a href="/schronisko">schronisko</a>
-<a href="/wolontariat">wolontariat</a>
+<nav>
+	<h2>adopcja</h2>
+	<ul>
+		<li><a href="/adopcja/czekaja-w-domach-tymczasowych">czekaja-w-domach-tymczasowych</a></li>
+		<li><a href="/adopcja/najpilniejsze-adopcje">najpilniejsze-adopcje</a></li>
+		<li><a href="/adopcja/one-czekaja-najdluzej">one-czekaja-najdluzej</a></li>
+		<li><a href="/adopcja/w-nowym-domu">w-nowym-domu</a></li>
+		<li><a href="/adopcja/za-teczowym-mostem">za-teczowym-mostem</a></li>
+	</ul>
+	<h2>schronisko</h2>
+	<ul>
+		<li><a href="/schronisko/aktualnosci">aktualnosci</a></li>
+		<li><a href="/schronisko/kontakt">kontakt</a></li>
+		<li><a href="/schronisko/o-schronisku">o-schronisku</a></li>
+		<li><a href="/schronisko/polityka-prywatnosci">polityka-prywatnosci</a></li>
+		<li><a href="/schronisko/przyjecia-zwierzat">przyjecia-zwierzat</a></li>
+	</ul>
+	<h2>wolontariat</h2>
+	<ul>
+		<li><a href="/wolontariat/informator-dla-nowych-wolontariuszy">informator-dla-nowych-wolontariuszy</a></li>
+		<li><a href="/wolontariat/jak-mozesz-pomoc">jak-mozesz-pomoc</a></li>
+		<li><a href="/wolontariat/nasi-przyjaciele">nasi-przyjaciele</a></li>
+		<li><a href="/wolontariat/o-wolontariacie">o-wolontariacie</a></li>
+		<li><a href="/wolontariat/wspolpracuja-z-nami">wspolpracuja-z-nami</a></li>
+	</ul>
+  <h2>zobacz</h2>
+	<ul>
+		<li><a href="/zobacz/adopcje-wirtualne">adopcje-wirtualne</a></li>
+		<li><a href="/zobacz/dlaczego-warto-adoptowac-staruszka">dlaczego-warto-adoptowac-staruszka</a></li>
+		<li><a href="/zobacz/przed-adopcja">przed-adopcja</a></li>
+		<li><a href="/zobacz/po-adopcji">po-adopcji</a></li>
+	</ul>
+</nav>
 <slot />
